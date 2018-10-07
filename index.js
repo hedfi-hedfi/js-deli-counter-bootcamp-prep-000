b4f5fab600c21de86141d923f94d21d032b94c26
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 18:36:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(peopleLine, newName) {
   peopleLine.push(newName);
-  return `Welcome, ${newName}. You are number ${peopleLine.length+1} in line.`
+  return `Welcome, ${newName}. You are number ${peopleLine.length} in line.`
 }
