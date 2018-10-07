fc856af14d983e1c13ec411d680cfb6053deba0d
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 18:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ function nowServing(peopleLine) {
 }
 
 function currentLine(peopleLine) {
-  if(peopleLine.length === 0) return 'The line is currently empty.'
-  return `The line is currently: ${peopleLine}.`
+  if(peopleLine.length === 0) return 'The line is currently empty.';
+  const list = [];
+  for (let i = 0; i < peopleLine.length; i++){
+    list.push(`${i+1}. ${peopleLine[i]}`)
+  }
+  return `The line is currently: ${list.join(','}.`;
 }
