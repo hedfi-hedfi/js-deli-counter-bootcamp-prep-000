85692df7a6f454d8794a2d56aff83f92e89857dd
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 18:38:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function takeANumber(peopleLine, newName) {
   peopleLine.push(newName);
   return `Welcome, ${newName}. You are number ${peopleLine.length} in line.`
 }
+
+function nowServing(peopleLine) {
+  if(peopleLine.length === 0) return 'There is nobody waiting to be served!'
+  return peopleLine.shift();
+}
