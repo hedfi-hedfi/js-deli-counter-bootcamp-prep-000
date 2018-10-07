4d0b1b3975dcd07e5debe1b56fcdf7930faf9c4c
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-07 18:42:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function nowServing(peopleLine) {
   if(peopleLine.length === 0) return 'There is nobody waiting to be served!'
   return `Currently serving ${peopleLine.shift()}.`;
 }
+
+function currentLine(peopleLine) {
+  if(peopleLine.length === 0) return 'The line is currently empty.'
+  return `The line is currently: ${peopleLine}.`
+}
